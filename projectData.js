301c1a0decdbff026dfd9b58e55013149ee5777e
David Nishikawa <triptype@gmail.com>
2017-08-31 16:54:54 -0700

    modified Project constructor; created new .js file for raw project data; created Handlebars.js framework

---
+++ projectData.js
@@ -0,0 +1,34 @@
+var rawProjectData = [
+  {
+    name: 'Beer Buddy',
+    dates: ['8-19-2017', '8-25-2017'],
+    context: '',
+    technologies: '',
+    url: '',
+    image: ''
+  },
+  {
+    name: 'Bus Mall',
+    dates: ['8-14-2017', '8-18-2017'],
+    context: '',
+    technologies: '',
+    url: '',
+    image: ''
+  },
+  {
+    name: 'Salmon Cookies',
+    dates: ['8-7-2017', '8-11-2017'],
+    context: '',
+    technologies: '',
+    url: '',
+    image: ''
+  },
+  {
+    name: 'About Me',
+    dates: ['7-31-2017', '8-6-2017'],
+    context: '',
+    technologies: '',
+    url: '',
+    image: ''
+  },
+];
